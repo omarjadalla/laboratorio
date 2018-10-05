8b1316141485ad9a04ba225461a6c5d8d33470c2
Omar Jadalla <omarmjadalla@gmail.com>
2018-10-04 21:40:49 -0300

    Distinct

---
+++ distinct.py
@@ -0,0 +1,21 @@
+def distinct(arr): 
+    
+    n = len(arr)
+    dist = []
+    
+    for i in range(0, n): 
+  
+        d = 0
+        for j in range(0, i): 
+            if (arr[i] == arr[j]): 
+                d = 1
+                break
+  
+        if (d == 0): 
+            dist.append(arr[i]) 
+    
+    return dist
+
+
+elementos = [1,0,2,1,3]    
+print( distinct(elementos) )
